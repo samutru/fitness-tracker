9583eff617d45d2afec69af04e0b735275292034
untereli <untereli@students.zhaw.ch>
2023-07-03 15:30:34 +0200

    added WorkoutEndpoint

---
+++ backend/src/main/java/ch/zhaw/sml/iwi/meng/leantodo/boundary/WorkoutEndpoint.java
@@ -0,0 +1,28 @@
+package ch.zhaw.sml.iwi.meng.leantodo.boundary;
+
+import java.security.Principal;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import ch.zhaw.sml.iwi.meng.leantodo.controller.WorkoutController;
+import ch.zhaw.sml.iwi.meng.leantodo.entity.Workout;
+
+@RestController
+@CrossOrigin
+public class WorkoutEndpoint {
+
+    @Autowired
+    WorkoutController workoutController;
+
+    @GetMapping("/workouts")
+    public List<Workout> getWorkouts() {
+         return workoutController.listAllWorkouts();
+    }
+
+    
+}
