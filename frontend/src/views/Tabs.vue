5b7797a48bd3bcbfe1ab6050a5979fb2c22afd8b
Samuel <samuel.truniger@hotmail.com>
2023-07-03 11:22:30 +0200

    Adjust paths for tabs

--- frontend/src/views/Tabs.vue
+++ frontend/src/views/Tabs.vue
@@ -3,21 +3,21 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="todo" href="/tabs/todo">
+        <ion-tab-button tab="workouts" href="/tabs/workouts">
           <ion-icon :icon="flash" />
           <ion-label>My Workouts</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="projects" href="/tabs/projects">
+        <ion-tab-button tab="exercises" href="/tabs/exercises">
           <ion-icon :icon="apps" />
           <ion-label>Exercises</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="timerecord" href="/tabs/timerecord">
+        <ion-tab-button tab="visualizations" href="/tabs/visualizations">
           <ion-icon :icon="send" />
           <ion-label>Visualizations</ion-label>
         </ion-tab-button>
-        <ion-tab-button tab="timerecord" href="/tabs/timerecord">
+        <ion-tab-button tab="profile" href="/tabs/profile">
           <ion-icon :icon="send" />
           <ion-label>Profile</ion-label>
         </ion-tab-button>
