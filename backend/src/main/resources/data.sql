e6bd1af12159605310b2eb7aefd59c15474b1b4d
Samuel <samuel.truniger@hotmail.com>
2023-07-03 14:53:18 +0200

    Add dummy data to data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,2 +1,12 @@
-INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Finish this app',false,false, 'user' );
-INSERT INTO TO_DO (title,archived,done,owner) VALUES ('Another task',false,false, 'user' );
+INSERT INTO EXERCISE_INFO (id, name, description, category, youtube_Link) VALUES 
+(1, 'Push-ups', 'Keep your body straight and lower it by bending your arms. Push up until your arms are straight.', 'Strength', 'https://youtube.com/v/push-ups'),
+(2, 'Squats', 'Stand straight with feet hip-width apart, and bend your knees as if you were going to sit.', 'Strength', 'https://youtube.com/v/squats');
+
+INSERT INTO WORKOUT (workout_Id, date_Of_Workout, total_Time, calories) VALUES
+(1, '2023-07-01', 60, 250.0),
+(2, '2023-07-02', 45, 200.0);
+
+INSERT INTO EXERCISE (exercise_Id, reps, exercise_Time, Workout_Id, exercise_Info_Id) VALUES
+(1, 10, 5, 1, 1),
+(2, 15, 10, 1, 2),
+(3, 10, 5, 2, 1);
