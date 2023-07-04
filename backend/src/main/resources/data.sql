d028504447e3bcbf17113fc8e56ae5cdb0b36fdb
Samuel <samuel.truniger@hotmail.com>
2023-07-04 10:39:55 +0200

    Improve exercise tab

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,6 +1,17 @@
 INSERT INTO EXERCISE_INFO (id, name, description, category, youtube_Link) VALUES 
-(1, 'Push-ups', 'Keep your body straight and lower it by bending your arms. Push up until your arms are straight.', 'Strength', 'https://youtube.com/v/push-ups'),
-(2, 'Squats', 'Stand straight with feet hip-width apart, and bend your knees as if you were going to sit.', 'Strength', 'https://youtube.com/v/squats');
+(1, 'Push-ups', 'Keep your body straight and lower it by bending your arms. Push up until your arms are straight.', 'Upper Body', 'https://youtube.com/v/push-ups'),
+(2, 'Squats', 'Stand straight with feet hip-width apart, and bend your knees as if you were going to sit.', 'Legs', 'https://youtube.com/v/squats'),
+(3, 'Bicep Curls', 'Hold a weight in each hand and bend your elbows to lift the weights towards your shoulders.', 'Arms', 'https://youtube.com/v/bicep-curls'),
+(4, 'Tricep Dips', 'Lower your body by bending your elbows until your upper arms and forearms create a 90 degree angle.', 'Arms', 'https://youtube.com/v/tricep-dips'),
+(5, 'Lunges', 'Step forward with one foot and lower your body until your front knee is at a 90 degree angle.', 'Legs', 'https://youtube.com/v/lunges'),
+(6, 'Planks', 'Hold your body up with your forearms and toes, keeping your body straight and holding for as long as possible.', 'Abs', 'https://youtube.com/v/planks'),
+(7, 'Crunches', 'Lay on your back, bend your knees and lift your shoulders off the floor.', 'Abs', 'https://youtube.com/v/crunches'),
+(8, 'Pull-ups', 'Hold onto a bar and pull your body weight up towards the bar.', 'Upper Body', 'https://youtube.com/v/pull-ups'),
+(9, 'Overhead Press', 'Stand upright, lift a barbell or dumbbells from your shoulders until your arms are fully extended above you.', 'Upper Body', 'https://youtube.com/v/overhead-press'),
+(10, 'Deadlifts', 'Stand with your feet hip-width apart, bend at the waist, keep your back straight, and lift a barbell off the ground.', 'Legs', 'https://youtube.com/v/deadlifts'),
+(11, 'Leg Press', 'Sit in a leg press machine, place your feet on the platform and push the weight away from you using your legs.', 'Legs', 'https://youtube.com/v/leg-press'),
+(12, 'Bench Press', 'Lay on a bench, lower a barbell to your chest, and then press it back up until your arms are straight.', 'Upper Body', 'https://youtube.com/v/bench-press');
+
 
 INSERT INTO WORKOUT (id, date_Of_Workout, total_Time, calories) VALUES
 (1, '2023-07-01', 60, 250.0),
--- frontend/src/views/Exercises.vue
+++ frontend/src/views/Exercises.vue
@@ -4,29 +4,70 @@
       <ion-toolbar>
         <ion-title>Exercises</ion-title>
       </ion-toolbar>
+      <ion-toolbar>
+        <ion-searchbar></ion-searchbar>
+      </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Exercises</ion-title>
-        </ion-toolbar>
-      </ion-header>
+    <ion-content :fullscreen="true" class="ion-padding">
+      <ion-list>
+        <ion-item>
+          <ion-select aria-label="Bodypart" interface="popover" placeholder="Select Bodypart" >
+            <ion-select-option value="abs">Abs</ion-select-option>
+            <ion-select-option value="arms">Arms</ion-select-option>
+            <ion-select-option value="chest">Chest</ion-select-option>
+            <ion-select-option value="legs">Legs</ion-select-option>
+            <ion-select-option value="cancel">Cancel</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list>
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" v-for="exerciseInfo in exerciseInfos" :key="exerciseInfo.id">
+            <ion-card color="primary" style="height: 10rem">
+              <ion-card-header>
+                <ion-card-title style="text-align: center">{{ exerciseInfo.name }} - {{ exerciseInfo.category }}</ion-card-title>
+              </ion-card-header>
+              <ion-card-content>
+                <ion-button expand="block" @click="setOpen(true, exerciseInfo)" color="light">Open</ion-button>
+              </ion-card-content>
+            </ion-card>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
 
-      <ion-searchbar></ion-searchbar>
-
-      <ion-card :key="exerciseInfo.id" v-for="exerciseInfo in exerciseInfos">
-        <ion-card-header>
-          <ion-card-title>{{ exerciseInfo.name }}</ion-card-title>
-        </ion-card-header>
-        <ion-card-content>Category: {{ exerciseInfo.category }} --- Description: {{ exerciseInfo.description }} </ion-card-content>
-      </ion-card>
+      <ion-modal :is-open="isOpen">
+        <ion-header>
+          <ion-toolbar>
+            <ion-title v-if="selectedExercise">{{ selectedExercise.name }}</ion-title>
+            <ion-buttons slot="end">
+              <ion-button @click="setOpen(false, null)">Close</ion-button>
+            </ion-buttons>
+          </ion-toolbar>
+        </ion-header>
+        <ion-content class="ion-padding">
+          <h3>Description:</h3>
+          <p v-if="selectedExercise">{{ selectedExercise.description }}</p>
+          <h3>YouTube:</h3>
+          <p v-if="selectedExercise">{{ selectedExercise.youtubeLink }}</p>
+        </ion-content>
+      </ion-modal>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonSearchbar } from '@ionic/vue';
+import { IonList, IonItem, IonSelect, IonSelectOption, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonSearchbar, IonModal, IonButtons, IonButton, IonGrid, IonRow, IonCol } from '@ionic/vue';
 import { UseExerciseInfos } from '../composables/useExerciseInfos';
+import { ref } from 'vue';
 
 const { exerciseInfos, getExerciseInfos } = UseExerciseInfos();
+const isOpen = ref(false);
+const selectedExercise = ref<any>();
+
+const setOpen = (status: boolean, exerciseInfo: any) => {
+  isOpen.value = status;
+  selectedExercise.value = exerciseInfo;
+};
 </script>
+
+<style scoped></style>
