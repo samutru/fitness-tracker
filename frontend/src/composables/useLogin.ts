37934e7e8837d4fba8994293b7d00e37098c8299
Samuel <samuel.truniger@hotmail.com>
2023-07-05 14:33:56 +0200

    Adjust login behvaior

--- frontend/src/composables/useLogin.ts
+++ frontend/src/composables/useLogin.ts
@@ -3,25 +3,26 @@ import { ref } from 'vue';
 import { useRouter } from 'vue-router';
 
 export function useLogin() {
+  const username = ref('');
+  const password = ref('');
+  const router = useRouter();
 
-    const username = ref("");
-    const password = ref("");
-    const router = useRouter();
-
-    const login = async () => {
-        try {
-            const token = await getToken(username.value, password.value);
-            // TODO: do something meaningful with the token :-)
-            //console.log(token)
-            router.push('/tabs/profile')
-        } catch (error) {
-            console.log(error)
-        }
+  const login = async () => {
+    try {
+      const token = await getToken(username.value, password.value);
+      if (token.loginName && token.expiresAt && token.roles) {
+        router.push('/tabs/profile');        
+      } else {
+        alert("Invalid username or password!")
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return {
-        username,
-        password,
-        login
-    }
+  return {
+    username,
+    password,
+    login,
+  };
 }
